80266b67ef1e8527530218ab4f2a25528e4e3193
Udeshya Sharma <114618734+udeshya17@users.noreply.github.com>
2023-04-21 15:57:53 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/calculator.html");
+});
+
+app.post("/", function(req,res){
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1+num2;
+  res.send("the sum is : " +result);
+});
+
+app.get("/bmicalculator", function(req , res){
+  res.sendFile(__dirname +"/bmicalculator.html");
+});
+
+app.post("/bmicalculator",function(req,res){
+  var weight = parseFloat(req.body.Weight);
+  var height = parseFloat(req.body.Height);
+  var bmi = weight/(height*height);
+  res.send("the BMI is : "+ bmi);
+});
+
+app.listen(3000,function(){
+  console.log("server is connected with 3000");
+});
